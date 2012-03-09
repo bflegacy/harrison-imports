dfda2bede599c3efdcad5e43b014ca5862f91c04
Casey Rees <casey.rees@collid.com>
2012-03-09 13:42:18 -0700

    added rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectHarrisonImports::Application.config.secret_token = '79c17179c6ade5d111daebcf281ae675a44d83798e5edffd3ff351a436968f4b45b7a91fe33d93ca838230d31e2f7e97fe124b1b24d03bfd6fc99ee4ee5139c2'
